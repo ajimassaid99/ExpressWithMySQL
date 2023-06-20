e9a10274a8a514c2b3a53295c40a7ac10b9a3175
Fauzi Maulia <fauzi.200130242@mhs.unimal.ac.id>
2023-06-20 11:45:36 +0700

    Express js Product dan Article

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const router = require('./routes');
+const log = require('./middlewares/logger');
+
+app.use(log);
+app.use(express.urlencoded({extended:true}));
+app.use(router);
+app.use((req,res,next)=>{
+    res.send({
+        status:'Failed',
+        massage: 'Resource ' + req.originalUrl + '  Not Found'
+    });
+});
+
+app.listen(3000, () => console.log('Server is running on http://localhost:3000'));
